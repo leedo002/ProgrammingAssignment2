d9d08dad7049cf6ed6969991f40588094ac0cfd8
leedo002 <donald.lee@staples.com>
2016-10-05 07:43:36 -0600

    Completed the coding for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of this script is to create functions that work together to 
+##   retrieve the inverse of a matrix, calculating and storing that inverse
+##   if it has not already been done.
 
-## Write a short comment describing this function
+## The makeCacheMatrix function creates the getter and setter functions, 
+##   including setsolve and getsolve, which manage the storing and 
+##   retrieving of the matrix inverses.  It returns a list of the 
+##   individual functions created.
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
-
+## The cacheSolve function receives as input an object of class makeCacheMatrix,
+##   checks to see if its inverse of the matrix has been calculated and stored
+##   previously, and then either returns the previously calculated inverse or it
+##   calculates the inverse, stores it, and returns the inverse.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
